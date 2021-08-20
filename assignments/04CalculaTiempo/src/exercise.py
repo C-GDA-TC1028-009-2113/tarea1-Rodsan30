7a5f331fec1bce20fab3dbf440563bb983e9238a
Rodsan30 <a01642309@tec.mx>
2021-08-20 18:39:32 +0000

    Tarea 1: Programas que realizan Calculos

--- assignments/04CalculaTiempo/src/exercise.py
+++ assignments/04CalculaTiempo/src/exercise.py
@@ -1,6 +1,9 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    edad = int(input("Dame tu edad: "))
+    año = int(input("Dame el año actual: "))
+    edad_futura = (100-edad+año)
+    print("Cumplirás 100 años en el año: "+str(edad_futura))
 
 
 
--- assignments/06Peso/src/exercise.py
+++ assignments/06Peso/src/exercise.py
@@ -1,6 +1,11 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    peso_inicial = float(input("Dame el peso inicial: "))
+    peso_final = float(input("Dame el peso final: "))
+    meses = int(input("Dame la cantidad de meses: "))
+    meta = (peso_inicial-peso_final)/meses
+    print("Lo que debes bajar por mes es: "+str(meta))
+
 
 
 
